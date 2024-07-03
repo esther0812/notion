56732c4264f277320d41bab0b63cec16a83c6085
cybermusk <cybermusk@usf.edu>
2024-07-03 15:30:55 +0800

    Update index.js

--- themes/hexo/index.js
+++ themes/hexo/index.js
@@ -275,7 +275,7 @@ const LayoutSlug = props => {
             const article = document.getElementById('notion-article')
             if (!article) {
               router.push('/404').then(() => {
-                console.warn('找不到页面', router.asPath)
+                console.warn('Page Lost', router.asPath)
               })
             }
           }
