5e1b7ee4b2bce020fd2d312b8e2fb93ccb0f2460
cybermusk <cybermusk@usf.edu>
2024-07-05 18:03:57 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'SugarDaddyMeet', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'DE GROOTSTE SPECIFIEKE SUGAR DADDY-DATINGSITE TER WERELD', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://is.gd/VMsPZx', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.sugardaddymeet.com?tid=af100028782-m', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Sugar Daddy, Sugar Baby, België', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
