606a76e3c46a12db19a3e1bf672aed31b5ada82f
cybermusk <cybermusk@usf.edu>
2024-07-08 11:52:17 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -75,7 +75,7 @@ const nextConfig = {
     : {
         defaultLocale: BLOG.LANG.slice(0, 2),
         // 支持的所有多语言,按需填写即可
-        locales: ['nl', 'en']
+        locales: ['en', 'nl']
       },
   images: {
     // 图片压缩
