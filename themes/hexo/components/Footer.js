2455dae349561bb6f12cecd979df805bd5b9fd4a
cybermusk <cybermusk@usf.edu>
2024-07-09 15:20:02 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -6,25 +6,51 @@ const Footer = ({ title }) => {
   const since = siteConfig('SINCE')
   const copyrightDate = parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
-  return (
-    <footer
-      className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm p-6'
-    >
-      {/* <DarkModeButton/> */}
-
-      <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={siteConfig('LINK')} className='underline font-bold  dark:text-gray-300 '>{siteConfig('AUTHOR')}</a>.<br/>
-
-      {siteConfig('BEI_AN') && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{siteConfig('BEI_AN')}</a><br/></>}
-
+return (
+  <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6 text-gray-600 dark:text-gray-100 text-sm p-6'>
+    {/* <DarkModeButton/> */}
+    <i className='fas fa-copyright' /> {`${copyrightDate}`}
+    <span>
+      <i className='mx-1 animate-pulse fas fa-heart' />{' '}
+      <a href={siteConfig('LINK')} className='underline font-bold dark:text-gray-300'>
+        {siteConfig('AUTHOR')}
+      </a>
+      .<br />
+      {siteConfig('BEI_AN') && (
+        <>
+          <i className='fas fa-shield-alt' />{' '}
+          <a href='https://beian.miit.gov.cn/' className='mr-2'>
+            {siteConfig('BEI_AN')}
+          </a>
+          <br />
+        </>
+      )}
       <span className='hidden busuanzi_container_site_pv'>
-            <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
+        <i className='fas fa-eye' />
+        <span className='px-1 busuanzi_value_site_pv'> </span>
+      </span>
       <span className='pl-2 hidden busuanzi_container_site_uv'>
-        <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
-        <p className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</p>
-        <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>Copyright © 2024 <a href='https://www.sugarpapa.be' className='dark:text-gray-300'>www.sugarpapa.be</a>, INC. All rights reserved.</p></span></br>
-        <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>Disclaimer: You must be over 19 to join. SugarPapa is not an escort service. SugarPapa does not support escorts or prostitution. Profiles that may violate these terms and services will be banned.</p>
-    </footer>
-  )
+        <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
+      </span>
+      <p className='text-xs pt-4 text-light-400 dark:text-gray-400'>
+        {title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}
+      </p>
+      <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>
+        Copyright © 2024{' '}
+        <a href='https://www.sugarpapa.be' className='dark:text-gray-300'>
+          www.sugarpapa.be
+        </a>
+        , INC. All rights reserved.
+      </p>
+      <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>
+        Disclaimer: You must be over 19 to join. SugarPapa is not an escort service. SugarPapa
+        does not support escorts or prostitution. Profiles that may violate these terms and
+        services will be banned.
+      </p>
+    </span>
+  </footer>
+);
+
 }
 
 export default Footer
