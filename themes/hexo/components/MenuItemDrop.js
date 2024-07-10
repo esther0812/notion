81bda45375cc881056abacbc049a1b9cb0f95835
cybermusk <cybermusk@usf.edu>
2024-07-10 11:33:55 +0800

    Update MenuItemDrop.js

--- themes/hexo/components/MenuItemDrop.js
+++ themes/hexo/components/MenuItemDrop.js
@@ -48,10 +48,12 @@ export const MenuItemDrop = ({ link }) => {
                 key={index}
                 className='cursor-pointer hover:bg-indigo-500 hover:text-white tracking-widest transition-all duration-200 dark:border-gray-800  py-1 pr-6 pl-3'>
                 <Link href={sLink.href} target={link?.target}>
+                 <a ref={linkRef}>
                   <span className='text-sm text-nowrap font-extralight'>
                     {link?.icon && <i className={sLink?.icon}> &nbsp; </i>}
                     {sLink.title}
                   </span>
+                 </a>
                 </Link>
               </li>
             )
