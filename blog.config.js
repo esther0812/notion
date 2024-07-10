51c622f1a28be445b008d176f3433c5c15bfe019
cybermusk <cybermusk@usf.edu>
2024-07-10 15:11:32 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Welkom, dit is de grootste Sugar Daddy datingwebsite ter wereld🔥, Registreer nu, en begin gratis met daten👩‍❤‍👨',
+    'Welkom, dit is de grootste Sugar Daddy datingwebsite ter wereld🔥, Registreer nu, begin gratis met daten👩‍❤‍👨',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
