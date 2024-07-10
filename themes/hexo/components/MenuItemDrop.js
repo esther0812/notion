d976a866693e8d71969bfb1c402b16a66a44239f
cybermusk <cybermusk@usf.edu>
2024-07-10 12:10:53 +0800

    Update MenuItemDrop.js

--- themes/hexo/components/MenuItemDrop.js
+++ themes/hexo/components/MenuItemDrop.js
@@ -39,24 +39,24 @@ export const MenuItemDrop = ({ link }) => {
 
       {/* 子菜单 */}
       {hasSubMenu && (
-       <ul
-        style={{ backdropFilter: 'blur(3px)' }}
-        className={`${show ? 'visible opacity-100 top-12' : 'invisible opacity-0 top-20'} drop-shadow-md overflow-hidden rounded-md text-black dark:text-white bg-white dark:bg-black transition-all duration-300 z-20 absolute block`}
-      >
-        {link.subMenus.map((sLink, index) => (
-          <li
-            key={index}
-            className='cursor-pointer hover:bg-indigo-500 hover:text-white tracking-widest transition-all duration-200 dark:border-gray-800  py-1 pr-6 pl-3'
-          >
-            <Link href={sLink.href} passHref>
-              <a className='text-sm text-nowrap font-extralight'>
-                {link?.icon && <i className={sLink?.icon}> &nbsp; </i>}
-                {sLink.title}
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+        <ul
+          style={{ backdropFilter: 'blur(3px)' }}
+          className={`${show ? 'visible opacity-100 top-12' : 'invisible opacity-0 top-20'} drop-shadow-md overflow-hidden rounded-md text-black dark:text-white bg-white dark:bg-black transition-all duration-300 z-20 absolute block  `}>
+          {link.subMenus.map((sLink, index) => {
+            return (
+              <li
+                key={index}
+                className='cursor-pointer hover:bg-indigo-500 hover:text-white tracking-widest transition-all duration-200 dark:border-gray-800  py-1 pr-6 pl-3'>
+                <Link href={sLink.href} target={link?.target}>
+                  <span className='text-sm text-nowrap font-extralight'>
+                    {link?.icon && <i className={sLink?.icon}> &nbsp; </i>}
+                    {sLink.title}
+                  </span>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
       )}
     </div>
   )
