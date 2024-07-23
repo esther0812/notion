e7be92fe97e409141384b71c43633b344d2631f3
cybermusk <cybermusk@usf.edu>
2024-07-23 17:36:11 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,7 +25,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'SugarDaddyMeet', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'DE GROOTSTE SPECIFIEKE SUGAR DADDY-DATINGSITE TER WERELD', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || 'أكبر موقع مواعدة مخصص لأب السكر في العالم', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.sugardaddymeet.com?tid=af100028782-m', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Sugar Daddy, Sugar Baby, الإمارات العربية المتحدة', // 网站关键词 英文逗号隔开
 
