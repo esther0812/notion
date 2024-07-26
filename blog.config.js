547df3d84b5475bdb1436f5f93913ddb9e3e7456
withAI777 <162964986+withAI777@users.noreply.github.com>
2024-07-26 11:46:51 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,14 +20,14 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hello, this is the biggest Sugar Daddy dating site in the world🔥, register now, start dating for free👩‍❤‍👨',
+    'Hello, This is a Notion template🔥, For reference only, Thank you👩‍❤‍👨',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'SugarDaddyMeet', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'أكبر موقع مواعدة مخصص لأب السكر في العالم', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || 'The largest sugar daddy dating site in the world', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.sugardaddymeet.com?tid=af100028782-m', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Sugar Daddy, Sugar Baby, الإمارات العربية المتحدة', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Sugar Daddy, Sugar Baby', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'sugardaddymeet2007@gmail.com', // 邮箱地址 例如mail@tangly1024.com
