66c3f7d06a14306ffea55ba86f22ccde92a6dd30
esther0812 <BunzNaismith865@gmail.com>
2024-09-13 16:22:19 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hello, This is a Notion template🔥, For reference only, Thank you👩‍❤‍👨',
+    'Hello, This is a herpes-only dating community🔥, For Herpes only, Thank you👩‍❤‍👨',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
