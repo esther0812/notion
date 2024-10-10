857a8ca227150c82450781b6d9b88de73fb0f4d0
esther0812 <BunzNaismith865@gmail.com>
2024-10-10 16:30:19 +0800

    Update Hero.js

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -96,7 +96,7 @@ const Hero = props => {
         id='header-cover'
         alt={siteInfo?.title}
         src={siteInfo?.pageCover}
-        className={`header-cover w-full h-auto object-cover object-center ${siteConfig('HEXO_HOME_NAV_BACKGROUND_IMG_FIXED', null, CONFIG) ? 'fixed' : ''}`}
+        className={`header-cover w-full h-screen object-cover object-center ${siteConfig('HEXO_HOME_NAV_BACKGROUND_IMG_FIXED', null, CONFIG) ? 'fixed' : ''}`}
       />
     </header>
   )
